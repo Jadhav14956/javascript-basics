68c5fcdbad1463fb63a3bf965326cda755573f95
suniljadhav709 <suniljadhav709@gmail.com>
2017-08-03 02:12:46 -0400

    Concepts

---
+++ scope.js
@@ -0,0 +1,3 @@
+//  ********************  Scope  *********************** //
+// the need for a well-defined set of rules for storing variables in some location, and for finding those variables at a later time. We'll call that set of rules: Scope.
+//  ********************  Scope  *********************** //
